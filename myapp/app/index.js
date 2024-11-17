e976664d3941125ccd901f1c7a7fc1165430c290
kritiarora2003 <kritiarora002@gmail.com>
2024-11-17 18:13:02 +0530

    Challenge-page-done

--- myapp/app/index.js
+++ myapp/app/index.js
@@ -2,11 +2,12 @@ import { View } from 'react-native';
 import Greeting from './Greeting';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
+import Challenge from './Challenge'
 
 export default function Page() {
   return (
     <View style={{ flex: 1 }}>
-      <SignUp />
+      <Challenge />
     </View>
   );
 }
